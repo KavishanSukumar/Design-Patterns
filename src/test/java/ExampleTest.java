ac97175bccb72213e6668f39c4a1eddaedc62027
KavishanSukumar <kavishansukumar@gmail.com>
2023-12-20 13:25:45 +0530

    project initialization

---
+++ src/test/java/ExampleTest.java
@@ -0,0 +1,9 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class ExampleTest {
+    @Test
+    void shouldShowSimpleAssertion(){
+        Assertions.assertEquals(1,2);
+    }
+}
